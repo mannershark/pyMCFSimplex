2bb38f2db37d73e7420ef11a27359a9f5b4ad3f6
Arjan van den Boogaart <avandenboogaart@thepeoplegroup.nl>
2022-07-12 16:14:09 +0200

    Move from distutils to setuptools

--- setup.py
+++ setup.py
@@ -4,16 +4,8 @@
 setup.py file for SWIG Wrapper for MCFSimplex
 """
 
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 import numpy as np  # for numpy array conversion
-
-# with open("pyMCFSimplex.i", "r") as file:
-#     content = file.read()
-#     _version = ''
-#     for line in content:
-#         if '__version__' in line:
-#             print(line)
-#             _version = line.split(' = ')[1].rstrip('"').lstrip('"')
 
 with open("README.md", "r") as file:
     long_desc = file.read()
@@ -21,9 +13,9 @@ with open("README.md", "r") as file:
 pyMCFSimplex_module = Extension('_pyMCFSimplex',
                                 sources=['pyMCFSimplex_wrap.cxx', 'MCFSimplex.cpp'],
                                 include_dirs=[np.get_include()], extra_compile_args=["-w"])
-print(np.get_include())
+
 setup(name='pyMCFSimplex',
-      version='0.9.3',
+      version='0.9.4',
       author="G#.Blog - Johannes Sommer",
       author_email="info@sommer-forst.de",
       url="http://www.sommer-forst.de/blog",
@@ -35,4 +27,5 @@ setup(name='pyMCFSimplex',
       license="LGPL 2.1",
       platforms=["win32", "manylinux1_x86_64"],
       py_modules=["pyMCFSimplex"],
+      install_requires=["numpy"],
       )
